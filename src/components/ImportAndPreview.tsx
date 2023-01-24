73007fc998f02a001185f5d9392208021cb7bd03
Borko Smolovic <borko.smolovic@gmail.com>
2023-01-24 18:26:02 +0100

    Import multiple items

--- src/components/ImportAndPreview.tsx
+++ src/components/ImportAndPreview.tsx
@@ -9,138 +9,147 @@ interface tumbnailDetails {
   type: string;
 }
 
+interface importedFile {
+  tumbnailDetails: tumbnailDetails;
+  isImage: boolean;
+  filePreview: string | ArrayBuffer | null | undefined;
+}
+
 export default function ImportAndPreview() {
-  const [imagePreview, setImagePreview] = useState<
-    string | ArrayBuffer | null | undefined
-  >(null);
-  const [videoPreview, setVideoPreview] = useState<
-    string | ArrayBuffer | null | undefined
-  >(null);
+  const [importedFiles, setImportedFiles] = useState<importedFile[]>([]);
   const [openPreview, setOpenPreview] = useState<boolean>(false);
-  const [tumbnailDetails, setTumbnalDetails] = useState<tumbnailDetails>({
+  const [tempTumbnail, setTempTumbnail] = useState<tumbnailDetails>({
     resolution: "",
     duration: 0,
     size: 0,
     type: "",
   });
-  const videoRef = useRef(null);
+  const videoRef = useRef<Array<HTMLVideoElement | HTMLImageElement | null>>(
+    []
+  );
 
   function previewFile(e: React.FormEvent<HTMLInputElement>) {
     const reader: FileReader = new FileReader();
-    clearFiles();
     if (!e.target) return;
-
     const target = e.target as HTMLInputElement;
-
     if (!target.files) return;
-
     const selectedFile = target.files[0];
-
     if (selectedFile) {
       reader.readAsDataURL(selectedFile);
     }
+    let allFiles = [];
     reader.onload = (readerEvent) => {
       if (!readerEvent.target) return;
-      if (selectedFile.type.includes("image")) {
-        setImagePreview(readerEvent.target.result);
-      } else if (selectedFile.type.includes("video")) {
-        setVideoPreview(readerEvent.target.result);
-      }
-      setDetails(readerEvent, selectedFile);
+      if (!importedFiles) return;
+      allFiles = [
+        ...importedFiles,
+        {
+          tumbnailDetails: getTumbnailDetails(readerEvent, selectedFile),
+          isImage: selectedFile.type.includes("image") ? true : false,
+          filePreview: readerEvent.target.result,
+        },
+      ];
+      setImportedFiles(allFiles);
+
+      // setDetails(readerEvent, selectedFile);
     };
   }
 
-  function setDetails(
+  function getTumbnailDetails(
     readerEvent: ProgressEvent<FileReader>,
     selectedFile: File
   ) {
-    if (!readerEvent || !readerEvent.target) return;
+    let tumbnailDetails = {
+      resolution: "",
+      duration: 0,
+      size: 0,
+      type: "",
+    };
+    if (!readerEvent || !readerEvent.target) return tumbnailDetails;
 
     if (selectedFile.type.includes("image")) {
       let img: any = new Image();
       img.src = readerEvent.target.result;
+      console.log("UDJEL#########333333333333", img.width);
       img.onload = function () {
-        setTumbnalDetails({
+        return {
           resolution: ((img.width as string) + "x" + img.height) as string,
           duration: 0,
           size: selectedFile.size,
           type: selectedFile.type,
-        });
+        };
       };
-    } else if (selectedFile.type.includes("video")) {
-      setTumbnalDetails({
-        resolution: tumbnailDetails.resolution,
-        duration: tumbnailDetails.duration,
-        size: selectedFile.size,
-        type: selectedFile.type,
-      });
     }
+    return tumbnailDetails;
   }
 
   function preview(fullSize: boolean, customClass?: string) {
-    if (imagePreview != null) {
-      return (
-        <img src={imagePreview as string} alt="" className={customClass} />
-      );
-    }
-
-    if (videoPreview != null) {
-      return (
-        <video
-          onLoadedMetadata={handleVideoMetadata}
-          ref={videoRef}
-          controls={fullSize ? true : false}
-          src={videoPreview as string}
-          className={customClass}
-        ></video>
-      );
-    }
-
-    return;
+    let items: any = [];
+    importedFiles?.map((item, index) => {
+      if (item.isImage) {
+        items.push(
+          <div className="import-and-preview__thumbnail_item" key={index}>
+            <span>
+              <img
+                src={item.filePreview as string}
+                alt=""
+                className={customClass}
+              />
+            </span>
+            <span>
+              <ul>
+                <li>Resolution: {item.tumbnailDetails.resolution}</li>
+                <li>
+                  Size: {Math.round(item.tumbnailDetails.size / 10000) / 100}MB
+                </li>
+                <li>Type: {item.tumbnailDetails.type}</li>
+              </ul>
+            </span>
+          </div>
+        );
+      } else {
+        items.push(
+          <div className="import-and-preview__thumbnail_item" key={index}>
+            <span>
+              <video
+                onLoadedMetadata={() => handleVideoMetadata(index)}
+                ref={(el) => (videoRef.current[index] = el)}
+                controls={fullSize ? true : false}
+                src={item.filePreview as string}
+                className={customClass}
+              ></video>
+            </span>
+            <span>
+              <ul>
+                <li>Resolution: {item.tumbnailDetails.resolution}</li>
+                <li>Duration: {item.tumbnailDetails.duration}s</li>
+                <li>
+                  Size: {Math.round(item.tumbnailDetails.size / 10000) / 100}MB
+                </li>
+                <li>Type: {item.tumbnailDetails.type}</li>
+              </ul>
+            </span>
+          </div>
+        );
+      }
+    });
+    console.log("test", items);
+    return items;
   }
 
-  function handleVideoMetadata() {
-    const video = videoRef.current;
+  function handleVideoMetadata(index: number) {
+    const video = videoRef.current[index];
     if (!video) return;
 
-    setTumbnalDetails({
-      resolution:
-        (video as HTMLVideoElement).videoWidth +
-        "x" +
-        (video as HTMLVideoElement).videoHeight,
-      duration: Math.round((video as HTMLVideoElement).duration * 100) / 100,
-      size: tumbnailDetails.size,
-      type: tumbnailDetails.type,
-    });
-  }
-
-  function details() {
-    if (!tumbnailDetails) return;
-
-    if (imagePreview != null) {
-      return (
-        <ul>
-          <li>Resolution: {tumbnailDetails.resolution}</li>
-          <li>Size: {Math.round(tumbnailDetails.size / 10000) / 100}MB</li>
-          <li>Type: {tumbnailDetails.type}</li>
-        </ul>
-      );
-    }
-    if (videoPreview != null) {
-      return (
-        <ul>
-          <li>Resolution: {tumbnailDetails.resolution}</li>
-          <li>Duration: {tumbnailDetails.duration}s</li>
-          <li>Size: {Math.round(tumbnailDetails.size / 10000) / 100}MB</li>
-          <li>Type: {tumbnailDetails.type}</li>
-        </ul>
-      );
-    }
-  }
-
-  function clearFiles() {
-    setImagePreview(null);
-    setVideoPreview(null);
+    // setTumbnalDetails({
+    //   resolution:
+    //     (video as HTMLVideoElement).videoWidth +
+    //     "x" +
+    //     (video as HTMLVideoElement).videoHeight,
+    //   duration: Math.round((video as HTMLVideoElement).duration * 100) / 100,
+    //   size: tumbnailDetails.size,
+    //   type: tumbnailDetails.type,
+    // });
   }
 
   return (
@@ -150,10 +159,12 @@ export default function ImportAndPreview() {
         <input accept="image/*, video/*" onChange={previewFile} type="file" />
       </div>
       <div>
-        <span onClick={() => setOpenPreview(true)}>
+        <span
+          onClick={() => setOpenPreview(true)}
+          className="import-and-preview__thumbnail_container"
+        >
           {preview(false, "import-and-preview__thumbnail")}
         </span>
-        {details()}
       </div>
 
       <Modal
